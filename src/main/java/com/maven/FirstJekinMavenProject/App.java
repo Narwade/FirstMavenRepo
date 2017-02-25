6cf00021ab3d539f59da78f5e2a3fd5d5319e19b
Narwade <nikhilnarwade@yahoo.in>
2017-02-25 22:03:50 +0530

    First commit

---
+++ src/main/java/com/maven/FirstJekinMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.maven.FirstJekinMavenProject;
+
+/**
+ * Hello Nikhil!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Nikhil!" );
+    }
+}
